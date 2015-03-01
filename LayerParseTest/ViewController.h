397723a7b05dbcb4ced32b11106d7f4df5794439
Abir Majumdar <beersy@gmail.com>
2015-03-01 12:08:09 -0800

    Initial Commit

---
+++ LayerParseTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayerParseTest
+//
+//  Created by Abir Majumdar on 3/1/15.
+//  Copyright (c) 2015 Abir Majumdar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LayerParseTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LayerParseTest
+//
+//  Created by Abir Majumdar on 3/1/15.
+//  Copyright (c) 2015 Abir Majumdar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
